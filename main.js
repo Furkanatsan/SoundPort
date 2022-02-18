17a430bcbf6feda6cb4e7a9e36fc7a5950ce73e5
furkan <furkanatsan@hotmail.com>
2022-02-18 16:44:01 +0300

    second commit

--- main.js
+++ main.js
@@ -7,30 +7,22 @@ const sound=document.getElementById("sound");
 
 //image
 const image = document.getElementById("image");
-
 booBtn.onclick=function () {
     sound.src="./sounds/boo.mp3"
     image.src="./images/booing.gif"
     document.body.style.backgroundColor="Chocolate";
     sound.play();
 }
-
 sound.onended=function () {
     image.src="./images/sound.png";
     document.body.style.backgroundColor="blueviolet";
     
 }
-
 applauseBtn.onclick=function () {
     sound.src="./sounds/applause.mp3"
     image.src="./images/applause.gif"
     document.body.style.backgroundColor="Chocolate"
     sound.play();
-}
-sound.onended=function () {
-    image.src="./images/sound.png";
-    document.body.style.backgroundColor="blueviolet"
-    
 }
 gaspBtn.onclick=function () {
     sound.src="./sounds/gasp.mp3"
@@ -38,20 +30,11 @@ gaspBtn.onclick=function () {
     document.body.style.backgroundColor="Chocolate"
     sound.play();
 }
-sound.onended=function () {
-    image.src="./images/sound.png";
-    document.body.style.backgroundColor="blueviolet"
-    
-}
 tadaBtn.onclick=function () {
     sound.src="./sounds/tada.mp3"
     image.src="./images/tada.gif"
     document.body.style.backgroundColor="Chocolate"
     sound.play();
-}
-sound.onended=function () {
-    image.src="./images/sound.png";
-    document.body.style.backgroundColor="blueviolet"
 }
 victoryBtn.onclick=function () {
     sound.src="./sounds/victory.mp3"
@@ -59,18 +42,10 @@ victoryBtn.onclick=function () {
     document.body.style.backgroundColor="Chocolate"
     sound.play();
 }
-sound.onended=function () {
-    image.src="./images/sound.png";
-    document.body.style.backgroundColor="blueviolet"
-}
 wrongBtn.onclick=function () {
     sound.src="./sounds/wrong.mp3"
     image.src="./images/wrong.gif"
     document.body.style.backgroundColor="Chocolate"
     sound.play();
-}
-sound.onended=function () {
-    image.src="./images/sound.png";
-    document.body.style.backgroundColor="blueviolet"
 }
     
